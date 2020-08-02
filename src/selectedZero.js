27f8a397e1aa840b7bb739d4e75d27c9e8bb06bb
Makoto Uwaya <makoto.uwaya@e-seikatsu.co.jp>
2020-08-02 14:51:04 +0900

    Remove menu pages ads

--- src/selectedZero.js
+++ src/selectedZero.js
@@ -1,16 +1,7 @@
 'use strict';
 
 function main() {
-  // Find keyselecter and set value
-  const keyselect = document.getElementsByName('keyselect')[0];
-  if (!keyselect) {
-    return;
-  }
-  keyselect.value = 0;
-  const event = new Event('change');
-  keyselect.dispatchEvent(event);
-
-  // Remove ads
+  // Remove mask ads
   let iconFindLimit = 5;
   while (iconFindLimit > 0) {
     var icon = document.getElementById('gn_interstitial_close_icon');
@@ -19,11 +10,12 @@ function main() {
       break;
     }
     setTimeout(() => {
-      console.log('waiting icon');
+      console.debug('waiting icon');
     }, 500);
     iconFindLimit--;
   }
 
+  // Remove overlay ads
   let commercialFindLimit = 5;
   while (commercialFindLimit > 0) {
     var commercial = document.querySelectorAll("[id^='gn_delivery_']")[0];
@@ -32,18 +24,43 @@ function main() {
       break;
     }
     setTimeout(() => {
-      console.log('waiting commercial');
+      console.debug('waiting commercial');
     }, 500);
 
     commercialFindLimit--;
   }
 
+  // Remove carousel ads
+  let carouselFindLimit = 5;
+  while (carouselFindLimit > 0) {
+    var carousel = document.getElementById('carouselExampleIndicators');
+    if (!!carousel) {
+      carousel.hidden = true;
+      break;
+    }
+    setTimeout(() => {
+      console.debug('waiting carousel');
+    }, 500);
+
+    carouselFindLimit--;
+  }
+
+  // Remove ads
   const ads = Array.from(document.querySelectorAll("[id^='criteo_slot_']"));
   const ioc = Array.from(document.getElementsByClassName('inst_overlap_cover'));
   const gap = Array.from(document.getElementsByClassName('google-auto-placed'));
   for (const ad of ads.concat(ioc).concat(gap)) {
     ad.hidden = true;
   }
+
+  // Find keyselecter and set value
+  const keyselect = document.getElementsByName('keyselect')[0];
+  if (!keyselect) {
+    return;
+  }
+  keyselect.value = 0;
+  const event = new Event('change');
+  keyselect.dispatchEvent(event);
 
   // Start play
   var chord = document.getElementById('my-chord-data');
