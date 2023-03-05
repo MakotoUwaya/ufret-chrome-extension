3bc22e9b67d71a46df05d5a07a9a099992d75e00
Makoto Uwaya <makoto.uwaya@e-seikatsu.co.jp>
2023-03-06 01:13:31 +0900

    feat: Allow u-fret site operations

--- src/content/index.tsx
+++ src/content/index.tsx
@@ -8,13 +8,7 @@ import { proxyStore as store } from '../app/proxyStore';
 
 import Content from './Content';
 
-withProxyStore(<Content />, store).then((component) => {
-  const container = document.createElement('my-extension-root');
-  document.body.append(container);
-  createRoot(container).render(component);
-});
-
-async function withProxyStore(children: ReactElement, proxyStore: Store): Promise<ReactElement> {
+const withProxyStore = async (children: ReactElement, proxyStore: Store): Promise<ReactElement> => {
   return proxyStore.ready().then(() => {
     return (
       <Provider store={proxyStore}>
@@ -23,3 +17,80 @@ async function withProxyStore(children: ReactElement, proxyStore: Store): Promis
     );
   });
 }
+
+withProxyStore(<Content />, store).then((component) => {
+  const container = document.createElement('my-extension-root');
+  document.body.append(container);
+  createRoot(container).render(component);
+});
+
+const main = (): void => {
+  // Remove mask ads
+  let iconFindLimit = 5;
+  while (iconFindLimit > 0) {
+    var icon = document.getElementById('gn_interstitial_close_icon');
+    if (icon) {
+      icon.click();
+      break;
+    }
+    setTimeout(() => {
+      console.debug('waiting icon');
+    }, 500);
+    iconFindLimit--;
+  }
+
+  // Remove carousel ads
+  let carouselFindLimit = 5;
+  while (carouselFindLimit > 0) {
+    var carousel = document.getElementById('carouselExampleIndicators');
+    if (carousel) {
+      carousel.remove();
+      break;
+    }
+    setTimeout(() => {
+      console.debug('waiting carousel');
+    }, 500);
+
+    carouselFindLimit--;
+  }
+
+  // Remove ads
+  const billboardAds = Array.from(document.querySelectorAll("[id^='div-gpt-ad-PC/']"));
+  const cfAsync = Array.from(document.querySelectorAll("[id^='cf_async_']"));
+  const mainAds = Array.from(document.querySelectorAll("[id^='google_ads_']"));
+  const bottomAds = Array.from(document.getElementsByClassName('adsbygoogle'));
+  const overlayAds = Array.from(document.getElementsByClassName('asOverlayAd__wrap'));
+  const googleAds = Array.from(document.querySelectorAll("[id^='google_ads_iframe_']"));
+  const gap = Array.from(document.getElementsByClassName('google-auto-placed'));
+  const allAds = [
+    ...billboardAds,
+    ...cfAsync,
+    ...mainAds,
+    ...bottomAds,
+    ...overlayAds,
+    ...googleAds,
+    ...gap,
+    document.querySelector('body > div.container > div:nth-child(11) > div:nth-child(4) > div:nth-child(19)'),
+    document.querySelector('body > div.container > div:nth-child(11) > div:nth-child(4) > div:nth-child(25)'),
+    document.getElementById('showAdPosition'),
+  ];
+  for (const ads of allAds) {
+    ads?.remove();
+  }
+
+  // Find keyselecter and set value
+  const keyselect: any = document.getElementsByName('keyselect')[0];
+  if (!keyselect) {
+    return;
+  }
+  keyselect.value = 0;
+  const event = new Event('change');
+  keyselect.dispatchEvent(event);
+
+  // Start play
+  var chord = document.getElementById('my-chord-data');
+  chord?.click();
+  keyselect.scrollIntoView();
+}
+
+main();
--- src/manifest.ts
+++ src/manifest.ts
@@ -2,15 +2,16 @@ import { ManifestV3Export } from '@crxjs/vite-plugin';
 
 const manifest: ManifestV3Export = {
   manifest_version: 3,
-  name: 'Browser Extension TypeScript & React Starter',
-  description: 'Browser Extension, TypeScript, React',
-  version: '0.1',
+  name: 'U-Fret utility',
+  description:
+    'U-Fret is a website with simple music scores. This Chrome extension provides a convenient way to use this site.',
+  version: '0.1.0',
   background: {
     service_worker: 'src/background/index.ts',
   },
   content_scripts: [
     {
-      matches: ['http://*/*', 'https://*/*', 'file:///*'],
+      matches: ['https://www.ufret.jp/*'],
       js: ['src/content/index.tsx'],
     },
   ],
@@ -31,19 +32,22 @@ const manifest: ManifestV3Export = {
   action: {
     default_popup: 'src/popup/popup.html',
     default_icon: {
-      '16': 'images/extension_16.png',
-      '32': 'images/extension_32.png',
-      '48': 'images/extension_48.png',
-      '128': 'images/extension_128.png',
+      '16': 'images/icon16.png',
+      '32': 'images/icon32.png',
+      '48': 'images/icon48.png',
+      '128': 'images/icon128.png',
     },
   },
   icons: {
-    '16': 'images/extension_16.png',
-    '32': 'images/extension_32.png',
-    '48': 'images/extension_48.png',
-    '128': 'images/extension_128.png',
+    '16': 'images/icon16.png',
+    '32': 'images/icon32.png',
+    '48': 'images/icon48.png',
+    '128': 'images/icon128.png',
   },
-  permissions: ['storage', 'tabs'],
+  permissions: ['storage', 'tabs', 'activeTab', 'declarativeContent'],
+  content_security_policy: {
+    extension_pages: "script-src 'self'; object-src 'self'",
+  },
 };
 
 export default manifest;
